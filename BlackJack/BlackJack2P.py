03afca6b05e6feb845946d497fcf4a13f40b913d
thiago <tgullo97@gmail.com>
2024-11-24 18:13:10 -0300

    upa

---
+++ BlackJack/BlackJack2P.py
@@ -0,0 +1,105 @@
+import gym
+from blackjack import BlackjackEnv
+from tqdm import tqdm
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+from sklearn.naive_bayes import GaussianNB
+
+# função para ver se ganhou
+def win(hand, dealer_hand):
+    if hand > 21:
+        return -1
+    elif dealer_hand > 21:
+        return 1
+    else:
+        if hand > dealer_hand:
+            return 1
+        elif hand < dealer_hand:
+            return -1
+        else:
+            return 0
+
+# Configurando Ambientes
+player_2_env = gym.make('Blackjack-v1', render_mode = "human", natural=True, sab=False)
+env = BlackjackEnv(render_mode="human")
+
+# Modelo Final de arquivo pkl
+import pickle
+model = pickle.load(open('../arvore_blackjack.sav', 'rb'))
+
+#teste 
+import keyboard
+
+# Guardar as informações
+p1_win = []
+p2_win = []
+dealer_hand = 0
+
+## Parte da Jogatina
+
+play = True
+
+while play is True:
+
+    # Requisitos do Player 2 (maquina treinada)
+    obs_p2, info = player_2_env.reset()
+
+    # Requisitos do Player 1
+    player_2, dealer_value, _ = obs_p2
+
+    # Reset para inicializar as variáveis do ambiente
+    obs, info = env.reset(dealer_hand=dealer_value)
+    env.render(player_2)
+    
+    done_done = False
+    done1 = False
+
+    # para cada partida
+    while not done_done:
+
+        #Primeiro Jogatina do Player 1 com tecla
+        if not done1:
+            #imaginando que se clicar S o valor eh 0 e se clicar H o valor é 1
+            print("Sua vez! Pressione 'H' para HIT ou 'S' para STAND.")
+
+            # Aguarda a ação do jogador
+            while True:
+                if keyboard.is_pressed('H'):
+                    player_action = 1  # HIT
+                    break
+                elif keyboard.is_pressed('S'):
+                    player_action = 0  # STAND
+                    break
+            #player_action = algum deles 
+
+            obs_p1, reward, terminated, truncated, _ = env.step(player_action)
+            env.render(player_2)
+            done1 = terminated or truncated
+
+            dealer_hand = env.get_dealer_sum()
+
+        else:
+            next_obs, reward, terminated, truncated, _ = player_2_env.step(model.predict([obs_p2])[0])
+            env.render(next_obs[0])
+            # Se terminou ou truncou (limite de tempo)
+            done_done = terminated or truncated
+
+            obs_p2 = next_obs
+    
+    env.render(obs_p2[0], done=True)
+
+    p1_win.append(win(obs_p1[0], dealer_hand))
+    p2_win.append(win(obs_p2[0], dealer_hand))
+
+    # Botao de reiniciar na tecla F
+    # quando clicado volta, se não fica em loop infinito esperando reiniciar
+    print("Partida finalizada. Pressione 'F' para reiniciar ou 'Q' para sair.")
+    
+    # Aguarda o comando para reiniciar ou sair
+    while True:
+        if keyboard.is_pressed('F'):
+            break  # Sai do loop interno e reinicia a partida
+        elif keyboard.is_pressed('Q'):
+            play = False  # Encerra o jogo
+            break
---
+++ BlackJack/Treinamento.py
@@ -0,0 +1,136 @@
+import gym
+from tqdm import tqdm
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+from sklearn.naive_bayes import GaussianNB
+
+# Classe de Black Jack
+
+class BlackjackAgent:
+
+    def __init__(self):
+        self.X_card = []
+        self.y_card = []
+        self.model = GaussianNB()
+        self.learning_Gap = 0
+        self.training_error = []        
+        self.lose = 0
+        self.win = 0
+
+    def get_action(self, observation):
+
+        if (self.learning_Gap <= 80):
+
+            if (observation[0] < 17):
+                return 1
+            
+            return 0
+        
+        return self.model.predict(np.array(observation).reshape(1, -1))[0]
+
+    def update(self, observation, reward, done, action):
+            
+            # A melhor decisão
+
+            if reward == -1:
+
+                # Se o modelo pediu para parar e perdeu por soma do dealer > player então deveria ter continuado
+                if action == 0:
+                    self.y_card.append(1)
+
+                # Se o modelo pediu para continuar mas burstou então deveria ter parado
+                else:
+                    self.y_card.append(0)
+
+
+            # Se teve empate (ainda não acabou) ou venceu então o modelo acertou
+            if reward >= 0:
+                self.y_card.append(action)
+
+
+            self.X_card.append(observation)
+            
+            if (self.learning_Gap % 80 == 0 and self.learning_Gap > 0):
+
+                self.model.fit(self.X_card, self.y_card)
+
+            if(done):
+                self.learning_Gap += 1
+
+                if (reward == -1):
+                    self.lose +=1
+                if (reward > 0):
+                    self.win +=1
+
+            
+            self.training_error.append(self.lose)
+        
+        
+# Preparano o Ambiente 
+env = gym.make('Blackjack-v1', render_mode = "human", natural=True, sab=False)
+env.reset()
+env.render()
+
+# Loop de Treinamento
+
+agent = BlackjackAgent()
+n_episodes = 20
+env = gym.wrappers.RecordEpisodeStatistics(env, deque_size=n_episodes)
+
+for episode in tqdm(range(n_episodes)):
+    obs, info = env.reset()
+    done = False
+    
+    # para cada partida
+    while not done:
+        action = agent.get_action(obs)
+        next_obs, reward, terminated, truncated, _ = env.step(action)
+
+        # Se terminou ou truncou (limite de tempo)
+        done = terminated or truncated
+
+        # Atualiza o agente
+        agent.update(obs, reward, done, action)
+
+        obs = next_obs
+
+env.close()    
+print("Treinamento Concluido")
+
+# mostra Resumo do Treinamento
+
+fig, axs = plt.subplots(ncols=2, figsize=(12, 5))
+
+axs[0].set_title("Penalidade por tentativas")
+axs[0].plot(range(len(agent.training_error)), agent.training_error)
+axs[0].set_ylabel("Penalidade")
+axs[0].set_xlabel("Tentativa")
+
+# Criar os dados para o gráfico
+data = {'Result': ['Win', 'Lose', 'Draw'], 'Count': [agent.win, agent.lose, n_episodes - (agent.win + agent.lose)]}
+
+# Plotar o gráfico de barras usando seaborn
+axs[1] = sns.barplot(x='Result', y='Count', data=data)
+
+# Adicionar rótulos nas barras
+for index, value in enumerate(data['Count']):
+    axs[1].text(index, value + 1, str(value), ha='center', fontweight='bold')
+    
+# Adicionando legendas e rótulos
+axs[1].set_xlabel("Resultado")
+axs[1].set_ylabel("Número de Partidas")
+axs[1].set_title("Número de Vitórias vs Derrotas")
+
+plt.tight_layout()
+plt.show()
+
+# Montar arvore de decisão
+from sklearn.tree import DecisionTreeClassifier
+
+classificador_BJ = DecisionTreeClassifier(criterion ='entropy')
+classificador_BJ.fit(agent.X_card, agent.y_card)
+
+import pickle
+
+pickle.dump(classificador_BJ, open('../Streamlit/arvore_blackjack.sav', 'wb'))
