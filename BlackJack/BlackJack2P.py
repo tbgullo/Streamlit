5f76cd6b01164be92541ff0f508591559828baf0
thiago <tgullo97@gmail.com>
2024-11-25 11:16:12 -0300

    update

--- BlackJack/BlackJack2P.py
+++ BlackJack/BlackJack2P.py
@@ -36,6 +36,35 @@ def setup_environment():
 # Função principal para a jogatina
 def play_game(player_2_env, env, p1_win, p2_win):
 
+    if "game_state" not in st.session_state:
+        # Requisitos do Player 2 (máquina treinada)
+        obs_p2, info = player_2_env.reset()
+        player_2, dealer_value, _ = obs_p2
+
+        # Reset para inicializar as variáveis do ambiente
+        obs, info = env.reset(dealer_hand=dealer_value)
+        st.session_state.game_state = {
+            "obs_p2": obs_p2,
+            "obs_p1": obs_p1,
+            "player_2": player_2,
+            "dealer_value": dealer_value,
+            "dealer_hand": dealer_hand,
+            "done_done": False,
+            "done1": False,
+            "p1_win": p1_win,
+            "p2_win": p2_win,
+        }
+    
+    # Recupera o estado atual
+    game_state = st.session_state.game_state
+    obs_p2 = game_state["obs_p2"]
+    obs_p1 = game_state["obs_p1"]
+    player_2 = game_state["player_2"]
+    dealer_value = game_state["dealer_value"]
+    dealer_hand = game_state["dealer_hand"]
+    done_done = game_state["done_done"]
+    done1 = game_state["done1"]
+    
     image_placeholder = st.empty()
 
     # Requisitos do Player 2 (maquina treinada)
@@ -58,21 +87,28 @@ def play_game(player_2_env, env, p1_win, p2_win):
             col1, col2 = st.columns(2)  # Colunas para os botões "HIT" e "STICK"
 
             with col1:
-                hit_button = st.button("HIT")
+                hit_button = st.button("HIT", key="hit")
             with col2:
-                stick_button = st.button("STICK")
-            while True:
-                if hit_button:
-                    player_action = 1
-                    break
-                elif stick_button:
-                    player_action = 0
-                    break
+                stick_button = st.button("STICK", key="stick")
 
-            obs_p1, reward, terminated, truncated, _ = env.step(player_action)
-            image_array = env.render(player_2)
-            image_placeholder.image(Image.fromarray(np.uint8(image_array)))
-            done1 = terminated or truncated
+            if hit_button:
+                player_action = 1
+                obs_p1, reward, terminated, truncated, _ = env.step(player_action)
+                game_state["obs_p1"] = obs_p1
+                game_state["done1"] = terminated or truncated
+                game_state["dealer_hand"] = env.get_dealer_sum()
+                image_array = env.render(player_2)
+                image_placeholder.image(Image.fromarray(np.uint8(image_array)))
+
+            elif stick_button:
+                player_action = 0
+                obs_p1, reward, terminated, truncated, _ = env.step(player_action)
+                game_state["obs_p1"] = obs_p1
+                game_state["done1"] = terminated or truncated
+                game_state["dealer_hand"] = env.get_dealer_sum()
+                image_array = env.render(player_2)
+                image_placeholder.image(Image.fromarray(np.uint8(image_array)))
+
             
             dealer_hand = env.get_dealer_sum()
         else:
@@ -86,14 +122,28 @@ def play_game(player_2_env, env, p1_win, p2_win):
 
             obs_p2 = next_obs
 
-    p1_win.append(win(obs_p1[0], dealer_hand))
-    p2_win.append(win(obs_p2[0], dealer_hand))
+    game_state["p1_win"].append(win(obs_p1[0], dealer_hand))
+    game_state["p2_win"].append(win(obs_p2[0], dealer_hand))
 
     image_array = env.render(obs_p2[0], done=True)
     image_placeholder.image(Image.fromarray(np.uint8(image_array)))
     # Botão de reiniciar exibido abaixo
-    if st.button("Reiniciar"):
-        st.experimental_rerun()
+    if st.button("Reiniciar", key="restart"):
+        # Apenas redefina o estado do jogo, sem reiniciar todo o script
+        obs_p2, info = player_2_env.reset()
+        player_2, dealer_value, _ = obs_p2
+        obs, info = env.reset(dealer_hand=dealer_value)
+
+        st.session_state.game_state = {
+            "obs_p2": obs_p2,
+            "obs": obs,
+            "player_2": player_2,
+            "dealer_value": dealer_value,
+            "done_done": False,
+            "done1": False,
+            "p1_win": [],
+            "p2_win": [],
+        }
 
 # Função para exibir o desempenho
 def show_performance(p1_win, p2_win):
