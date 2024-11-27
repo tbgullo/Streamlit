cc0940631eebb583958bcc16e57febeb0f04b634
thiago <tgullo97@gmail.com>
2024-11-26 22:51:29 -0300

    update

--- BlackJack/BlackJack2P.py
+++ BlackJack/BlackJack2P.py
@@ -131,6 +131,7 @@ def play_game(p1_win, p2_win):
                 log_message("dentro do done done")
                 action = model.predict(np.array(obs_p2).reshape(1, -1))[0]
                 print(action)
+                log_message(f"action {action}")
                 next_obs, reward, terminated, truncated, _ = player_2_env.step(action)
 
                 image_array = env.render(next_obs[0])
