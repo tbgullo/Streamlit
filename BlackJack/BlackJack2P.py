949d709849f60a20f7ad65f4c98abe394c96727d
thiago <tgullo97@gmail.com>
2024-11-27 21:31:16 -0300

    update final

--- BlackJack/BlackJack2P.py
+++ BlackJack/BlackJack2P.py
@@ -130,11 +130,13 @@ def play_game():
             while not done_done:
                     #next_obs, reward, terminated, truncated = None, None, None, None
 
-                    action = model.predict(np.array(obs_p2).reshape(1, -1))[0]
+                    
                     try:
+                        action = model.predict(np.array(obs_p2).reshape(1, -1))[0]
                         next_obs, reward, terminated, truncated, _ = player_2_env.step(action)
                     except AttributeError as e:
                         x = 0
+                    action = model.predict(np.array(obs_p2).reshape(1, -1))[0]
                     next_obs, reward, terminated, truncated, _ = player_2_env.step(action)
                     image_array = env.render(next_obs[0])
 
